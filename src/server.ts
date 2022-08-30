d6f81f37281b062ca8a4afb54a6bf2a79b1ce199
radl97 <radl97@users.noreply.github.com>
2022-08-31 00:00:38 +0200

    Auto-add bot as second player, trigger bot move after player moves

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { MyGame as ChessBishopsGame } from './games/chess-bishops/game';
 import { PostgresStore } from 'bgio-postgres';
 import { env } from 'process';
 import { gameWrapper } from './common/gamewrapper';
+import { BOT_ID, fetch, SocketIOButBotMoves } from './socketio_botmoves';
 
 function getDb() {
   if (env.DATABASE_URL) {
@@ -17,6 +18,9 @@ function getDb() {
   }
 }
 
+export function getBotCredentials() {
+  return env.BOT_CREDENTIALS || "botbotbot";
+}
 
 const server = Server({
   games: [
@@ -24,8 +28,40 @@ const server = Server({
     gameWrapper(SuperstitiousCountingGame),
     gameWrapper(ChessBishopsGame),
   ],
+  transport: new SocketIOButBotMoves({ https: undefined }),
   ...getDb(),
 })
 
 const PORT = parseInt(env.PORT || "8000");
+
+/** Joins a bot to all matches where the bot's side is not connected.
+ * 
+ * TODO inject bots only where it is needed.
+ * 
+ * This should be in line with boardgame.io/src/server/api.ts
+ * path would be '/games/:name/:id/join'.
+ */
+async function injectBots(db: any) {
+  console.log("Injecting listing matches!");
+  for (let matchID of await db.listMatches()) {
+    console.log(`Found match ${matchID}`);
+    var match = await fetch(db, matchID, {metadata: true});
+    // TODO do not connect a bot to ALL unconnected
+    if (!match.metadata.players[BOT_ID].isConnected) {
+      console.log(`Found empty match!`);
+      match.metadata.players[BOT_ID].name = 'Bot';
+      match.metadata.players[BOT_ID].credentials = getBotCredentials();
+      match.metadata.players[BOT_ID].isConnected = true;
+      await db.setMetadata(matchID, match.metadata);
+    }
+  }
+}
+
+/** This should be in line with boardgame.io/src/server/api.ts */
+server.router.post('/games/:nameid/create', async (ctx, next) => {
+  // TODO somehow figure out the MatchID of the created match
+  await next();
+  await injectBots(ctx.db);
+});
+
 server.run(PORT);
---
+++ src/socketio_botmoves.ts
@@ -0,0 +1,154 @@
+// Demultiplexes to real transport or bots
+
+import type { Game, State } from "boardgame.io";
+import { getFilterPlayerView } from "boardgame.io/internal";
+import { Master } from "boardgame.io/master";
+import { SocketIO } from "boardgame.io/server";
+import botWrapper from "./common/botwrapper";
+import { currentPlayer } from "./common/types";
+import { strategy } from "./games/tictactoe/strategy";
+import { getBotCredentials } from "./server";
+
+/** Copied from boardgame.io/dist/src/client/transport/local.ts */
+function GetBotPlayer(state: State, bots: Record<string, any>) {
+  if (state.ctx.gameover !== undefined) {
+    return null;
+  }
+
+  if (state.ctx.activePlayers) {
+    for (const key of Object.keys(bots)) {
+      if (key in state.ctx.activePlayers) {
+        return key;
+      }
+    }
+  } else if (state.ctx.currentPlayer in bots) {
+    return state.ctx.currentPlayer;
+  }
+
+  return null;
+}
+
+/** Copied from boardgame.io/dist/src/server/db/base.ts */
+export enum Type {
+  SYNC = 0,
+  ASYNC = 1,
+}
+
+/** Copied from boardgame.io/dist/src/server/transport.ts */
+export function isSynchronous(storageAPI: any): any {
+  return storageAPI.type() === Type.SYNC;
+}
+
+/** Used by TransportAPI. Copied from boardgame.io/dist/src/server/transport.ts */
+const emit = (socket: any, { type, args }: any) => {
+  socket.emit(type, ...args);
+};
+
+/** Used by TransportAPI. Copied from boardgame.io/dist/src/server/transport.ts */
+function getPubSubChannelId(matchID: string): string {
+  return `MATCH-${matchID}`;
+}
+
+/** Copied from boardgame.io/dist/src/server/transport.ts */
+const TransportAPI = (
+  matchID: string,
+  socket: any,
+  filterPlayerView: any,
+  pubSub: any
+): any => {
+  const send : (arg1: any, ...arg2: any) => void = ({ playerID, ...data }) => {
+    emit(socket, filterPlayerView(playerID, data));
+  };
+
+  const sendAll = (payload: any) => {
+    pubSub.publish(getPubSubChannelId(matchID), payload);
+  };
+
+  return { send, sendAll };
+};
+
+/** Copied from boardgame.io/dist/src/master/master.ts */
+export async function fetch(db: any, matchID: string, partial: Partial<{state: boolean, metadata: boolean}>) {
+  let state;
+  if (isSynchronous(db)) {
+    ({ state } = db.fetch(matchID, partial));
+  } else {
+    ({ state } = await db.fetch(matchID, partial));
+  }
+  return state;
+}
+
+/// Bot's playerID is '1', because the gameWrapper uses player '0' for the human player. 
+export const BOT_ID = '1';
+
+/** This is a special transport specifically designed for replacing a player's move
+ * with a bot's move.
+ * 
+ * BOT_ID represents the player to replace.
+ * 
+ * Modifying the server is also needed to fill the bot's slot in the lobby (see injectBots()).
+ */
+export class SocketIOButBotMoves extends SocketIO {
+  init(
+    app: any,
+    games: Game[],
+    origins: any
+  ) {
+    super.init(app, games, origins);
+
+    for (const game of games) {
+      const nsp = app._io.of(game.name);
+      const bot = new (botWrapper(strategy))({
+        enumerate: (game as any).ai.enumerate,
+        seed: game.seed,
+      });
+
+      /** This should be in sync with how socket data is communicated.
+       * See boardgame.io/dist/src/server/transport/socketio.ts
+       */
+      nsp.on('connection', (socket: any) => {
+        socket.on('update', async (...args: Parameters<any>) => {
+          // The arguments are stale: we react to a player's step
+          // But we are on the same API that reacts to it
+          // Basically we assume that a socket.on('update', ...)
+          // already updated the gamestate, making StateID and PlayerID stale
+          const [_, staleStateID, matchID, stalePlayerID] : any[] = args;
+          const matchQueue = this.getMatchQueue(matchID);
+          await matchQueue.add(async () => {
+            // These happen after the player stepped.
+            // The state is written to storage, and the server now returned
+            // the authorative state to the player.
+            // TODO do not load the result from storage, reuse from the redux?
+            // TODO try do not send an authorative state to the player...?
+            console.log("Bot moves");
+
+            if (stalePlayerID === BOT_ID) {
+              // Do not react to bot's turn
+              return;
+            }
+            const state = await fetch(app.context.db, matchID, {state: true});
+            if (state.ctx.phase !== 'play' || currentPlayer(state.ctx) !== BOT_ID) {
+              // Not a real action, possibly a failed move.
+              return;
+            }
+            const botAction = await bot.play(
+              state,
+              GetBotPlayer(state, {[BOT_ID]: bot}) as any
+            );
+            
+            const master = new Master(
+              game,
+              app.context.db,
+              TransportAPI(matchID, socket, getFilterPlayerView(game), this.pubSub),
+              app.context.auth
+            );
+
+            // TODO: is staleStateID+1 always valid?
+            let nextStateID = staleStateID+1;
+            await master.onUpdate({type: 'MAKE_MOVE', payload: {...botAction.action.payload, credentials: getBotCredentials()}}, nextStateID, matchID, BOT_ID);
+          });
+        });
+      });
+    }
+  }
+}
