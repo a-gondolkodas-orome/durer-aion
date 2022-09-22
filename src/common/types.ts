53e2caf764eb89e88bcd81011eb364372f31422d
Jánosik Áron <aron@janosik.hu>
2022-09-22 23:49:22 +0200

    fixing types
    fix typing again
    
    
    fix types again 2.0

--- src/common/types.ts
+++ src/common/types.ts
@@ -2,7 +2,7 @@ import { Ctx, MoveMap, TurnConfig } from "boardgame.io";
 
 export type PlayerIDType = "0" | "1";
 
-interface WrapperStateMixin {
+interface GameStateMixin {
   winner: PlayerIDType | "draw" | null;
   difficulty: null | undefined | string;
   numberOfTries: number;
@@ -13,10 +13,10 @@ interface WrapperStateMixin {
 
 /// GameWrapper's mixin.
 /// setup() is defined here, as it returns G instead of G & WrapperState 
-interface WrapperMixin<G> {
+interface GameMixin<MixG,G> {
   possibleMoves: (G: G, ctx: Ctx, playerID: PlayerIDType) => any[];
   setup: () => G,
-  startingPosition?: (_: {G: G; ctx: Ctx; playerID: PlayerIDType; random: any}) => G;
+  startingPosition?: (_: {G: MixG; ctx: Ctx; playerID: PlayerIDType; random: any}) => G;
 }
 
 /// Base structure, passed through directly to boardgame.io.
@@ -28,7 +28,7 @@ interface WrappableGame<G extends any = any, PluginAPIs extends Record<string, u
   turn?: TurnConfig<G, PluginAPIs>;
 }
 
-export type GameType<G> = WrappableGame<G & WrapperStateMixin> & WrapperMixin<G>;
+export type GameType<G> = WrappableGame<G & GameStateMixin> & GameMixin<G & GameStateMixin,G>;
 
 /// Allows typing: change ctx.currentPlayer -> currentPlayer(ctx)
 export function currentPlayer(ctx: Ctx): "0" | "1" {
