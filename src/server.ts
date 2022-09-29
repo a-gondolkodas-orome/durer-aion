2eda7787624ad3c3f7d3567d675d7158c47a24f4
radl97 <radl97@users.noreply.github.com>
2022-09-29 02:36:14 +0200

    Upgrade to my feature-bot (https://github.com/radl97/boardgame.io/pull/1)

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import { Server } from 'boardgame.io/server';
 import { MyGame as TicTacToeGame } from './games/tictactoe/game';
 import { MyGame as SuperstitiousCountingGame } from './games/superstitious-counting/game';
 import { MyGame as ChessBishopsGame } from './games/chess-bishops/game';
 import { PostgresStore } from 'bgio-postgres';
 import { env } from 'process';
 import { gameWrapper } from './common/gamewrapper';
-import { BOT_ID, fetch, SocketIOButBotMoves } from './socketio_botmoves';
+import { Server, SocketIO } from 'boardgame.io/server';
+import botWrapper from './common/botwrapper';
+import { strategy as TicTacToeStrategy } from './games/tictactoe/strategy';
+import { strategy as SuperstitiousCountingStrategy } from './games/superstitious-counting/strategy';
+import { strategy as ChessBishopsStrategy } from './games/chess-bishops/strategy';
 
 function getDb() {
   if (env.DATABASE_URL) {
@@ -22,50 +25,34 @@ export function getBotCredentials() {
   return env.BOT_CREDENTIALS || "botbotbot";
 }
 
+
+const games = [
+  gameWrapper(TicTacToeGame),
+  gameWrapper(SuperstitiousCountingGame),
+  gameWrapper(ChessBishopsGame),
+];
+
+const bot_factories : any = [
+  botWrapper(TicTacToeStrategy),
+  botWrapper(SuperstitiousCountingStrategy),
+  botWrapper(ChessBishopsStrategy),
+];
+
+const bots = games.map((game, idx) => new (bot_factories[idx])({
+  enumerate: game.ai?.enumerate,
+  seed: game.seed,
+}));
+
 const server = Server({
-  games: [
-    gameWrapper(TicTacToeGame),
-    gameWrapper(SuperstitiousCountingGame),
-    gameWrapper(ChessBishopsGame),
-  ],
-  transport: new SocketIOButBotMoves({ https: undefined }),
+  games: games,
+  transport: new SocketIO(
+    { https: undefined },
+    getBotCredentials(),
+    bots,
+  ),
   ...getDb(),
 })
 
 const PORT = parseInt(env.PORT || "8000");
-
-/** Joins a bot to all matches where the bot's side is not connected.
- * 
- * TODO inject bots only where it is needed.
- * 
- * This should be in line with boardgame.io/src/server/api.ts
- * path would be '/games/:name/:id/join'.
- */
-async function injectBots(db: any) {
-  console.log("Injecting listing matches!");
-  for (let matchID of await db.listMatches()) {
-    console.log(`Found match ${matchID}`);
-    var match = await fetch(db, matchID, {metadata: true});
-    // TODO do not connect a bot to ALL unconnected
-    // TODO broken
-    if (match === undefined) {
-      return;
-    }
-    if (!match.metadata.players[BOT_ID].isConnected) {
-      console.log(`Found empty match!`);
-      match.metadata.players[BOT_ID].name = 'Bot';
-      match.metadata.players[BOT_ID].credentials = getBotCredentials();
-      match.metadata.players[BOT_ID].isConnected = true;
-      await db.setMetadata(matchID, match.metadata);
-    }
-  }
-}
-
-/** This should be in line with boardgame.io/src/server/api.ts */
-server.router.post('/games/:nameid/create', async (ctx, next) => {
-  // TODO somehow figure out the MatchID of the created match
-  await next();
-  await injectBots(ctx.db);
-});
 
 server.run(PORT);
