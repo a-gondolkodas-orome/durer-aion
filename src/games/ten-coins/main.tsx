b3eb06565878575b7dffb4db5fbc292d04457a33
smate314 <soosmate01@gmail.com>
2022-10-05 15:00:41 +0200

    add game description

--- src/games/ten-coins/main.tsx
+++ src/games/ten-coins/main.tsx
@@ -4,16 +4,14 @@ import { MyBoard } from './board';
 import { strategy } from './strategy';
 
 let description = <p className="text-justify">
-  Ez a játék a XIII. Dürer döntőjén szerepelt E kategóriában.
-
-  Károly és Dezső m-ig szeretnének elszámolni, és közben a következő játékot játsszák:
-  0-ról kezdenek, a két játékos felváltva adhat hozzá egy 13-nál kisebb pozitív egészet a korábbi
-  számhoz, azonban a babonájuk miatt ha egyikük x-et adott hozzá, akkor másikuk a következő
-  lépésben nem adhat hozzá 13 − x-et. Az veszít, aki eléri (vagy átlépi) m-et.
+  Kezdetben van 10 érme az asztalon, melyek értékei 1 és N közé eső pozitív egészek lehetnek. 
+  Amikor te jössz, akkor azt csinálhatod, hogy kiválasztasz egy K értéket, amire létezik K értékű érme, 
+  és az összes K értékű érmét átváltoztatod valamilyen kisebb L értékűre (mindet ugyanarra az értékre).
+  Az nyer, akinek a lépése után minden érme azonos értékű lesz.
 
   Az új játék gombra kattintva generálhatsz egy felállást.
 
-  Az m szám ismeretében te döntheteted el, hogy a kezdő vagy a második játékos bőrébe szeretnél e bújni.
+  A kezdőállás szám ismeretében te döntheteted el, hogy a kezdő vagy a második játékos bőrébe szeretnél e bújni.
 </p>
 
 const TenCoins = MyClient(MyGame, MyBoard, strategy, description);
