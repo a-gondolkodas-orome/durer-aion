3189358b2d5d0d2c17c8494c8d07f3d8318e1436
OnionKiller <tomi@csilling.com>
2022-10-18 00:20:34 +0200

    add ctx types fix team guid routing clash fix return error

--- src/server/router.ts
+++ src/server/router.ts
@@ -3,8 +3,9 @@ import koaBody from 'koa-body';
 import type { Server } from 'boardgame.io';
 import { TeamsRepository } from './db';
 
-export function configureTeamsRouter(router: any, teams: TeamsRepository) {
-  router.get('/team/admin/filter', koaBody(), async (ctx: any) => {
+export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: TeamsRepository) {
+
+  router.get('/team/admin/filter', koaBody(), async (ctx: Server.AppCtx) => {
     const filter_string = ctx.request.query['filter'];
     let filters;
     if (filter_string === undefined) {
@@ -14,36 +15,38 @@ export function configureTeamsRouter(router: any, teams: TeamsRepository) {
     }
     ctx.body = await teams.fetch(filters);
     //    ctx.body = ['8eae8669-125c-42e5-8b49-89afbac31679', '18c3a69d-c477-4578-8dc1-6e430fbb4e80', '48df4969-a834-4131-ab75-24069a56d2d6'];
-  });
+  })
 
-  router.get('/team/join/:token', koaBody(), async (ctx: any) => {
+  router.get('/team/join/:token', koaBody(), async (ctx: Server.AppCtx) => {
     const connect_token: string = ctx.params.token ?? 'no-token';
     const team = await teams.getTeam({ joinCode: connect_token });
-    ctx.body = team?.credentials;
+    ctx.body = team?.id;
     if (team == null)
       ctx.throw(404, "Team not found!")
-  });
+  })
 
-
-  router.get('/team/:GUID', koaBody(), async (ctx: any) => {
+  router.get(/^\/team\/(?<GUID>[^-]{8}-[^-]{4}-[^-]{4}-[^-]{4}-[^-]{12}$)/, koaBody(), async (ctx: Server.AppCtx) => {
     const GUID = ctx.params.GUID ?? ctx.throw(400)
-    const team = await teams.getTeam({id:GUID}) ?? ctx.throw(404, `Team with {id:${GUID}} not found.`)
-    return team;
+    console.log(GUID);
+    const team = await teams.getTeam({ id: GUID }) ?? ctx.throw(404, `Team with {id:${GUID}} not found.`)
+    ctx.body = team;
   });
 
-  router.get('/team/:GUID/relay/play', koaBody(), async (ctx:Server.AppCtx) => {
+  router.get('/team/:GUID/relay/play', koaBody(), async (ctx: Server.AppCtx) => {
     ctx.throw(501, "Not implemented yet.");
   })
 
-  router.get('/team/:GUID/strategy/play', koaBody(), async (ctx:Server.AppCtx) => {
+  router.get('/team/:GUID/strategy/play', koaBody(), async (ctx: Server.AppCtx) => {
     ctx.throw(501, "Not implemented yet.");
   })
 
-  router.get('/team/:GUID/relay/result', koaBody(), async (ctx:Server.AppCtx) => {
+  router.get('/team/:GUID/relay/result', koaBody(), async (ctx: Server.AppCtx) => {
     ctx.throw(501, "Not implemented yet.");
   })
 
-  router.get('/team/:GUID/strategy/result', koaBody(), async (ctx:Server.AppCtx) => {
+  router.get('/team/:GUID/strategy/result', koaBody(), async (ctx: Server.AppCtx) => {
     ctx.throw(501, "Not implemented yet.");
   })
+
+
 }
