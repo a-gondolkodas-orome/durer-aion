3ba3e9424c0cfbca5f0cb65bd4fadba3f6cfa549
Ádám Fraknói <fraknoiadam@gmail.com>
2022-10-19 21:44:44 +0200

    Fix client factory in relay

--- src/common/client_factory.tsx
+++ src/common/client_factory.tsx
@@ -1,9 +1,32 @@
-import { MyClient, MyClientWithBot, MyOnlineClient } from "./myclient";
+import { MyClient, MyClientRelay, MyClientRelayWithBot, MyClientWithBot, MyOnlineClient, MyOnlineRelayClient } from "./myclient";
 
 export const ClientFactory = function (game: any, board: any, strategy: any, description: any) {
   const Client = MyClient(game, board, description);
   const ClientWithBot = MyClientWithBot(game, board, strategy, description);
   const OnlineClient = MyOnlineClient(game, board, description);
+  return {
+    Client: function () {
+      return (<>
+        <Client />
+      </>);
+    },
+    ClientWithBot: function () {
+      return (<>
+        <ClientWithBot playerID='0' />
+      </>);
+    },
+    OnlineClient: function ({ credentials, matchID }: { credentials: string, matchID: string }) {
+      return (<>
+        <OnlineClient playerID='0' credentials={credentials} matchID={matchID} />
+      </>);
+    }
+  };
+};
+
+export const ClientFactoryRelay = function (game: any, board: any, strategy: any, description: any) {
+  const Client = MyClientRelay(game, board, description);
+  const ClientWithBot = MyClientRelayWithBot(game, board, strategy, description);
+  const OnlineClient = MyOnlineRelayClient(game, board, description);
   return {
     Client: function () {
       return (<>
