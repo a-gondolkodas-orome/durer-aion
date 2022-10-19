5fa13f2feefa5cfa2ad0eec547a0309f6bbc356a
radl97 <radl97@users.noreply.github.com>
2022-10-19 19:37:55 +0200

    Fix? src/components/ReactClient.tsx

--- src/components/ReactClient.tsx
+++ src/components/ReactClient.tsx
@@ -2,12 +2,12 @@ import { RelayClientWithBot_C, RelayClientWithBot_D, RelayClientWithBot_E, Relay
 import { ClientWithBot_C, ClientWithBot_D, ClientWithBot_E } from '../games/ten-coins/main';
 
 
-const DURER_XVI_CLIENT_C_RELAY = RelayClientWithBot_C;
-const DURER_XVI_CLIENT_D_RELAY = RelayClientWithBot_D;
-const DURER_XVI_CLIENT_E_RELAY = RelayClientWithBot_E;
-const DURER_XVI_CLIENT_C_STRATEGY = ClientWithBot_C;
-const DURER_XVI_CLIENT_D_STRATEGY = ClientWithBot_D;
-const DURER_XVI_CLIENT_E_STRATEGY = ClientWithBot_E;
+const DURER_XVI_CLIENT_C_RELAY = RelayClientOnlineClient_C;
+const DURER_XVI_CLIENT_D_RELAY = RelayClientOnlineClient_D;
+const DURER_XVI_CLIENT_E_RELAY = RelayClientOnlineClient_E;
+const DURER_XVI_CLIENT_C_STRATEGY = ClientOnlineClient_C;
+const DURER_XVI_CLIENT_D_STRATEGY = ClientOnlineClient_D;
+const DURER_XVI_CLIENT_E_STRATEGY = ClientOnlineClient_E;
 
 export function DurerXVIRelayClient({ category, matchID, credentials }: {
   category: undefined | 'C' | 'D' | 'E', matchID: string,
