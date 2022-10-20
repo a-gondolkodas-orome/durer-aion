5799fa6b7d7aa78cb1c555a9407087d80cdc33f0
OnionKiller <tomi@csilling.com>
2022-10-20 04:16:38 +0200

    add condititonal buttons

--- src/common/boardwrapper.tsx
+++ src/common/boardwrapper.tsx
@@ -1,26 +1,30 @@
+import { Stack } from '@mui/system';
+import {Button} from '@mui/material'
+
 export function boardWrapper(board: any, description: any) { // TODO: solve types with BoardProps<MyGameState>
   return ({ G, ctx, moves, log }: any) => {
     return (
       <div>
-        <h1>boardgame.io Typescript Demo</h1>
         <p>{description}</p>
         <p>Az "Új próbajáték" gombra kattintva próbajáték indul, ami a pontozásba nem számít bele. Bátran kérjetek próbajátékot, hiszen ezzel tudjátok tesztelni, hogy jól értitek-e a játék működését. Az "Új éles játék" gombra kattintva indul a valódi játék, ami már pontért megy.</p>
-        <div>
-          <button
-            onClick={() => moves.chooseNewGameType("test")}
-          >Új próbajáték kezdése</button>
-          <button
-            onClick={() => moves.chooseNewGameType("live")}
-          >Új játék kezdése</button>
-        </div>
-        <div>
-          <button
-            onClick={() => moves.chooseRole(0)}
-          >Kezdő leszek</button>
-          <button
-            onClick={() => moves.chooseRole(1)}
-          >Második leszek</button>
-        </div>
+        {ctx.phase === 'startNewGame' && G.winner === null && (
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained"
+              onClick={() => moves.chooseNewGameType("test")}
+            >Új próbajáték kezdése</Button>
+            <Button variant="contained"
+              onClick={() => moves.chooseNewGameType("live")}
+            >Új játék kezdése</Button>
+          </Stack>)}
+          {ctx.phase === 'chooseRole' &&
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained"
+              onClick={() => moves.chooseRole(0)}
+            >Kezdő leszek</Button>
+            <Button variant="contained"
+              onClick={() => moves.chooseRole(1)}
+            >Második leszek</Button>
+          </Stack>}
         {ctx.phase === 'startNewGame' && G.winner === null && <p> Kezdj új játékot! </p>}
         {ctx.phase === 'chooseRole' && <p> Válaszd ki, hogy első vagy második játékos akarsz-e lenni. </p>}
         {ctx.phase === 'play' && ctx.currentPlayer === "0" && <p> Most Te jössz! </p>}
