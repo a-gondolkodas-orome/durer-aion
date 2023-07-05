09899e7bdb78f3890b6246dbfe3a7cb77117a36d
OnionKiller <tomi@csilling.com>
2023-07-05 23:10:56 +0200

    refactor: remove pure patchwork code, remenance of the original bypass.

--- src/codes.ts
+++
@@ -1,8 +0,0 @@
-import { TeamModelDto } from "./client/dto/TeamStateDto";
-import { teamData } from "./teamData";
-
-export const teams: TeamModelDto[] = teamData.map(team =>{ return{
-  joinCode:team.join_code,
-  category:team.category,
-  name:team.teamname}
-})
