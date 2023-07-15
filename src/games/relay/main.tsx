a7fbaa1bb4162a961f30aff68d0ca96de618b25e
radl97 <radl97@users.noreply.github.com>
2023-07-15 12:04:51 +0200

    Simplify relay main.tsx

--- src/games/relay/main.tsx
+++ src/games/relay/main.tsx
@@ -1,36 +1,11 @@
 import { GameRelay } from './game';
-import { MyBoard } from './board';
 import { strategy } from './strategy';
-import { Client } from 'boardgame.io/react';
-import { Debug } from 'boardgame.io/debug';
-import { Local } from 'boardgame.io/multiplayer';
-import botWrapper from '../../common/botwrapper';
 import { ClientFactoryRelay } from '../../common/client_factory';
 import { InProgressRelay } from '../../client/components/teamstates/InProgressRelay';
 
 let description = <p className="text-justify">
 </p>
 
-const Relay_C = Client({
-  game: GameRelay,
-  board: MyBoard,
-  numPlayers: 2,
-  debug: { impl: Debug },
-  multiplayer: Local(
-    {
-      bots: { '1': botWrapper(strategy("C")) }
-    }
-  ),
-});
-
 export const { Client:RelayClient_C, ClientWithBot:RelayClientWithBot_C, OnlineClient:RelayOnlineClient_C } = ClientFactoryRelay({...GameRelay, name: "relay_c"}, InProgressRelay, strategy("C"), description);
 export const { Client:RelayClient_D, ClientWithBot:RelayClientWithBot_D, OnlineClient:RelayOnlineClient_D } = ClientFactoryRelay({...GameRelay, name: "relay_d"}, InProgressRelay, strategy("D"), description);
 export const { Client:RelayClient_E, ClientWithBot:RelayClientWithBot_E, OnlineClient:RelayOnlineClient_E } = ClientFactoryRelay({...GameRelay, name: "relay_e"}, InProgressRelay, strategy("E"), description);
-
-export default function () {
-  return (
-    <>
-      <Relay_C playerID='0' />
-    </>
-  );
-};
