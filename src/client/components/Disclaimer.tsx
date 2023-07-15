37102abea6b4ebbbd5842d37cb12940b0af4c85d
david <david.roman@proglimes.com>
2023-07-15 19:59:14 +0200

    create seperate constant for texts

--- src/client/components/Disclaimer.tsx
+++ src/client/components/Disclaimer.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material';
 import { Stack } from '@mui/system';
 import React from 'react';
+import { dictionary } from '../text-constants';
 
 export function Disclaimer(props: {setAccomplished: React.Dispatch<boolean>}) {
   return (
@@ -17,7 +18,7 @@ export function Disclaimer(props: {setAccomplished: React.Dispatch<boolean>}) {
         fontSize: 16,
         marginBottom: "25px"
       }}>
-        Az online fordulón a csapatoknak önállóan kell dolgozniuk, külső segítséget nem fogadhatnak el, másokkal nem kommunikálhatnak egészen a versenyzési időszak végéig.
+        {dictionary.disclaimer.start}
       </Stack>
 
       <Stack sx={{
@@ -25,14 +26,14 @@ export function Disclaimer(props: {setAccomplished: React.Dispatch<boolean>}) {
         marginBottom: "10px",
         fontStyle: "italic",
       }}>
-        Továbbjutás
+        {dictionary.disclaimer.progress}
       </Stack>
       
       <Stack sx={{
         fontSize: 16,
         marginBottom: "150px",
       }}>
-        Azok a csapatok, amelyek az online forduló során a megszerezhető 52 pontból legalább 25 pontot elérnek, továbbjutnak a helyi fordulóba. (Fenntartjuk a jogot, hogy a ponthatárt esetleg csökkentsük, növelni biztosan nem fogjuk.) Az online fordulón szerzett pontszám nem számít bele a további eredményekbe.
+        {dictionary.disclaimer.progressDescription}
       </Stack>
 
       <Button sx={{
@@ -42,7 +43,7 @@ export function Disclaimer(props: {setAccomplished: React.Dispatch<boolean>}) {
         alignSelf: 'center',
         textTransform: 'none',
       }} variant='contained' color='primary' onClick={()=>{props.setAccomplished(true)}}>
-        Tovább a versenyhez
+        {dictionary.disclaimer.continue}
       </Button>
     </Stack>
   )
---
+++ src/client/text-constants.ts
@@ -0,0 +1,13 @@
+export const dictionary = {
+  disclaimer: {
+    start:
+      "Az online fordulón a csapatoknak önállóan kell dolgozniuk, külső segítséget nem fogadhatnak el, " +
+      "másokkal nem kommunikálhatnak egészen a versenyzési időszak végéig.",
+    progress: "Továbbjutás",
+    progressDescription:
+      "Azok a csapatok, amelyek az online forduló során a megszerezhető 52 pontból legalább 25 pontot " +
+      "elérnek, továbbjutnak a helyi fordulóba. (Fenntartjuk a jogot, hogy a ponthatárt esetleg csökkentsük, " +
+      "növelni biztosan nem fogjuk.) Az online fordulón szerzett pontszám nem számít bele a további eredményekbe.",
+    continue: "Tovább a versenyhez",
+  },
+};
