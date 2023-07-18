844e58520a2860c461e5f20acb59a5631ede3428
Ádám Fraknói <fraknoiadam@gmail.com>
2023-07-18 13:46:42 +0200

    add all relay problems

--- src/client/components/ReactClient.tsx
+++ src/client/components/ReactClient.tsx
@@ -1,4 +1,6 @@
-import { RelayClientWithBot_10_D_A, RelayClientWithBot_10_D_B, RelayClientWithBot_10_D_C, RelayOnlineClient_C, RelayOnlineClient_D, RelayOnlineClient_E } from "../../games/relay/main";
+
+import { RelayOnlineClient_C, RelayOnlineClient_D, RelayOnlineClient_E } from "../../games/relay/main";
+import { RelayClientWithBot_1_D_B, RelayClientWithBot_1_D_C, RelayClientWithBot_1_D_D, RelayClientWithBot_2_D_B, RelayClientWithBot_2_D_C, RelayClientWithBot_2_D_D, RelayClientWithBot_3_D_B, RelayClientWithBot_3_D_C, RelayClientWithBot_3_D_D, RelayClientWithBot_4_D_B, RelayClientWithBot_4_D_C, RelayClientWithBot_4_D_D, RelayClientWithBot_5_D_A, RelayClientWithBot_5_D_B, RelayClientWithBot_5_D_C, RelayClientWithBot_5_D_D, RelayClientWithBot_6_D_A, RelayClientWithBot_6_D_B, RelayClientWithBot_6_D_C, RelayClientWithBot_6_D_D, RelayClientWithBot_7_D_A, RelayClientWithBot_7_D_B, RelayClientWithBot_7_D_C, RelayClientWithBot_7_H_A, RelayClientWithBot_7_H_B, RelayClientWithBot_8_D_A, RelayClientWithBot_8_D_B, RelayClientWithBot_8_D_C, RelayClientWithBot_8_D_D, RelayClientWithBot_8_H_A, RelayClientWithBot_8_H_B, RelayClientWithBot_9_D_A, RelayClientWithBot_9_D_B, RelayClientWithBot_9_D_C, RelayClientWithBot_9_D_Cp, RelayClientWithBot_9_D_D, RelayClientWithBot_9_D_Dp, RelayClientWithBot_9_H_A, RelayClientWithBot_9_H_B, RelayClientWithBot_10_D_A, RelayClientWithBot_10_D_B, RelayClientWithBot_10_D_C, RelayClientWithBot_10_D_Cp, RelayClientWithBot_10_D_D, RelayClientWithBot_10_D_Dp, RelayClientWithBot_10_H_A, RelayClientWithBot_10_H_B, RelayClientWithBot_11_D_A, RelayClientWithBot_11_D_B, RelayClientWithBot_11_D_C, RelayClientWithBot_11_D_Cp, RelayClientWithBot_11_D_D, RelayClientWithBot_11_D_Dp, RelayClientWithBot_11_H_A, RelayClientWithBot_11_H_B, RelayClientWithBot_12_D_A, RelayClientWithBot_12_D_B, RelayClientWithBot_12_D_C, RelayClientWithBot_12_D_D, RelayClientWithBot_12_D_E, RelayClientWithBot_12_D_Ep, RelayClientWithBot_12_H_A, RelayClientWithBot_12_H_B } from "../../games/relay/main";
 import { OnlineClient_C as StrategyOnlineClient_C, OnlineClient_D as StrategyOnlineClient_D, OnlineClient_E as StrategyOnlineClient_E } from "../../games/ten-coins/main";
 
 const DURER_XVI_CLIENT_C_RELAY = RelayOnlineClient_C;
@@ -7,9 +9,70 @@ const DURER_XVI_CLIENT_E_RELAY = RelayOnlineClient_E;
 const DURER_XVI_CLIENT_C_STRATEGY = StrategyOnlineClient_C;
 const DURER_XVI_CLIENT_D_STRATEGY = StrategyOnlineClient_D;
 const DURER_XVI_CLIENT_E_STRATEGY = StrategyOnlineClient_E;
+
+const DURER_OLD_CLIENT_1_D_B = RelayClientWithBot_1_D_B;
+const DURER_OLD_CLIENT_1_D_C = RelayClientWithBot_1_D_C;
+const DURER_OLD_CLIENT_1_D_D = RelayClientWithBot_1_D_D;
+const DURER_OLD_CLIENT_2_D_B = RelayClientWithBot_2_D_B;
+const DURER_OLD_CLIENT_2_D_C = RelayClientWithBot_2_D_C;
+const DURER_OLD_CLIENT_2_D_D = RelayClientWithBot_2_D_D;
+const DURER_OLD_CLIENT_3_D_B = RelayClientWithBot_3_D_B;
+const DURER_OLD_CLIENT_3_D_C = RelayClientWithBot_3_D_C;
+const DURER_OLD_CLIENT_3_D_D = RelayClientWithBot_3_D_D;
+const DURER_OLD_CLIENT_4_D_B = RelayClientWithBot_4_D_B;
+const DURER_OLD_CLIENT_4_D_C = RelayClientWithBot_4_D_C;
+const DURER_OLD_CLIENT_4_D_D = RelayClientWithBot_4_D_D;
+const DURER_OLD_CLIENT_5_D_A = RelayClientWithBot_5_D_A;
+const DURER_OLD_CLIENT_5_D_B = RelayClientWithBot_5_D_B;
+const DURER_OLD_CLIENT_5_D_C = RelayClientWithBot_5_D_C;
+const DURER_OLD_CLIENT_5_D_D = RelayClientWithBot_5_D_D;
+const DURER_OLD_CLIENT_6_D_A = RelayClientWithBot_6_D_A;
+const DURER_OLD_CLIENT_6_D_B = RelayClientWithBot_6_D_B;
+const DURER_OLD_CLIENT_6_D_C = RelayClientWithBot_6_D_C;
+const DURER_OLD_CLIENT_6_D_D = RelayClientWithBot_6_D_D;
+const DURER_OLD_CLIENT_7_D_A = RelayClientWithBot_7_D_A;
+const DURER_OLD_CLIENT_7_D_B = RelayClientWithBot_7_D_B;
+const DURER_OLD_CLIENT_7_D_C = RelayClientWithBot_7_D_C;
+const DURER_OLD_CLIENT_7_H_A = RelayClientWithBot_7_H_A;
+const DURER_OLD_CLIENT_7_H_B = RelayClientWithBot_7_H_B;
+const DURER_OLD_CLIENT_8_D_A = RelayClientWithBot_8_D_A;
+const DURER_OLD_CLIENT_8_D_B = RelayClientWithBot_8_D_B;
+const DURER_OLD_CLIENT_8_D_C = RelayClientWithBot_8_D_C;
+const DURER_OLD_CLIENT_8_D_D = RelayClientWithBot_8_D_D;
+const DURER_OLD_CLIENT_8_H_A = RelayClientWithBot_8_H_A;
+const DURER_OLD_CLIENT_8_H_B = RelayClientWithBot_8_H_B;
+const DURER_OLD_CLIENT_9_D_A = RelayClientWithBot_9_D_A;
+const DURER_OLD_CLIENT_9_D_B = RelayClientWithBot_9_D_B;
+const DURER_OLD_CLIENT_9_D_C = RelayClientWithBot_9_D_C;
+const DURER_OLD_CLIENT_9_D_Cp = RelayClientWithBot_9_D_Cp;
+const DURER_OLD_CLIENT_9_D_D = RelayClientWithBot_9_D_D;
+const DURER_OLD_CLIENT_9_D_Dp = RelayClientWithBot_9_D_Dp;
+const DURER_OLD_CLIENT_9_H_A = RelayClientWithBot_9_H_A;
+const DURER_OLD_CLIENT_9_H_B = RelayClientWithBot_9_H_B;
 const DURER_OLD_CLIENT_10_D_A = RelayClientWithBot_10_D_A;
 const DURER_OLD_CLIENT_10_D_B = RelayClientWithBot_10_D_B;
 const DURER_OLD_CLIENT_10_D_C = RelayClientWithBot_10_D_C;
+const DURER_OLD_CLIENT_10_D_Cp = RelayClientWithBot_10_D_Cp;
+const DURER_OLD_CLIENT_10_D_D = RelayClientWithBot_10_D_D;
+const DURER_OLD_CLIENT_10_D_Dp = RelayClientWithBot_10_D_Dp;
+const DURER_OLD_CLIENT_10_H_A = RelayClientWithBot_10_H_A;
+const DURER_OLD_CLIENT_10_H_B = RelayClientWithBot_10_H_B;
+const DURER_OLD_CLIENT_11_D_A = RelayClientWithBot_11_D_A;
+const DURER_OLD_CLIENT_11_D_B = RelayClientWithBot_11_D_B;
+const DURER_OLD_CLIENT_11_D_C = RelayClientWithBot_11_D_C;
+const DURER_OLD_CLIENT_11_D_Cp = RelayClientWithBot_11_D_Cp;
+const DURER_OLD_CLIENT_11_D_D = RelayClientWithBot_11_D_D;
+const DURER_OLD_CLIENT_11_D_Dp = RelayClientWithBot_11_D_Dp;
+const DURER_OLD_CLIENT_11_H_A = RelayClientWithBot_11_H_A;
+const DURER_OLD_CLIENT_11_H_B = RelayClientWithBot_11_H_B;
+const DURER_OLD_CLIENT_12_D_A = RelayClientWithBot_12_D_A;
+const DURER_OLD_CLIENT_12_D_B = RelayClientWithBot_12_D_B;
+const DURER_OLD_CLIENT_12_D_C = RelayClientWithBot_12_D_C;
+const DURER_OLD_CLIENT_12_D_D = RelayClientWithBot_12_D_D;
+const DURER_OLD_CLIENT_12_D_E = RelayClientWithBot_12_D_E;
+const DURER_OLD_CLIENT_12_D_Ep = RelayClientWithBot_12_D_Ep;
+const DURER_OLD_CLIENT_12_H_A = RelayClientWithBot_12_H_A;
+const DURER_OLD_CLIENT_12_H_B = RelayClientWithBot_12_H_B;
 
 export function DurerXVIRelayClient({ category, matchID, credentials }: {
   category: undefined | 'C' | 'D' | 'E', matchID: string,
@@ -65,11 +128,72 @@ export function DurerOldRelayClient({ category, matchID, credentials }: {
   category: undefined | string, matchID: string,
   credentials: string
 }) {
+  const files = ["1_D_B","1_D_C","1_D_D","2_D_B","2_D_C","2_D_D","3_D_B","3_D_C","3_D_D","4_D_B","4_D_C","4_D_D","5_D_A","5_D_B","5_D_C","5_D_D","6_D_A","6_D_B","6_D_C","6_D_D","7_D_A","7_D_B","7_D_C","7_H_A","7_H_B","8_D_A","8_D_B","8_D_C","8_D_D","8_H_A","8_H_B","9_D_A","9_D_B","9_D_C","9_D_Cp","9_D_D","9_D_Dp","9_H_A","9_H_B", "10_D_A","10_D_B","10_D_C","10_D_Cp","10_D_D","10_D_Dp","10_H_A","10_H_B","11_D_A","11_D_B","11_D_C","11_D_Cp","11_D_D","11_D_Dp","11_H_A","11_H_B","12_D_A","12_D_B","12_D_C","12_D_D","12_D_E","12_D_Ep","12_H_A","12_H_B",];
   return (
     <>
+    { category === '1_D_B' && (<DURER_OLD_CLIENT_1_D_B/>)}
+    { category === '1_D_C' && (<DURER_OLD_CLIENT_1_D_C/>)}
+    { category === '1_D_D' && (<DURER_OLD_CLIENT_1_D_D/>)}
+    { category === '2_D_B' && (<DURER_OLD_CLIENT_2_D_B/>)}
+    { category === '2_D_C' && (<DURER_OLD_CLIENT_2_D_C/>)}
+    { category === '2_D_D' && (<DURER_OLD_CLIENT_2_D_D/>)}
+    { category === '3_D_B' && (<DURER_OLD_CLIENT_3_D_B/>)}
+    { category === '3_D_C' && (<DURER_OLD_CLIENT_3_D_C/>)}
+    { category === '3_D_D' && (<DURER_OLD_CLIENT_3_D_D/>)}
+    { category === '4_D_B' && (<DURER_OLD_CLIENT_4_D_B/>)}
+    { category === '4_D_C' && (<DURER_OLD_CLIENT_4_D_C/>)}
+    { category === '4_D_D' && (<DURER_OLD_CLIENT_4_D_D/>)}
+    { category === '5_D_A' && (<DURER_OLD_CLIENT_5_D_A/>)}
+    { category === '5_D_B' && (<DURER_OLD_CLIENT_5_D_B/>)}
+    { category === '5_D_C' && (<DURER_OLD_CLIENT_5_D_C/>)}
+    { category === '5_D_D' && (<DURER_OLD_CLIENT_5_D_D/>)}
+    { category === '6_D_A' && (<DURER_OLD_CLIENT_6_D_A/>)}
+    { category === '6_D_B' && (<DURER_OLD_CLIENT_6_D_B/>)}
+    { category === '6_D_C' && (<DURER_OLD_CLIENT_6_D_C/>)}
+    { category === '6_D_D' && (<DURER_OLD_CLIENT_6_D_D/>)}
+    { category === '7_D_A' && (<DURER_OLD_CLIENT_7_D_A/>)}
+    { category === '7_D_B' && (<DURER_OLD_CLIENT_7_D_B/>)}
+    { category === '7_D_C' && (<DURER_OLD_CLIENT_7_D_C/>)}
+    { category === '7_H_A' && (<DURER_OLD_CLIENT_7_H_A/>)}
+    { category === '7_H_B' && (<DURER_OLD_CLIENT_7_H_B/>)}
+    { category === '8_D_A' && (<DURER_OLD_CLIENT_8_D_A/>)}
+    { category === '8_D_B' && (<DURER_OLD_CLIENT_8_D_B/>)}
+    { category === '8_D_C' && (<DURER_OLD_CLIENT_8_D_C/>)}
+    { category === '8_D_D' && (<DURER_OLD_CLIENT_8_D_D/>)}
+    { category === '8_H_A' && (<DURER_OLD_CLIENT_8_H_A/>)}
+    { category === '8_H_B' && (<DURER_OLD_CLIENT_8_H_B/>)}
+    { category === '9_D_A' && (<DURER_OLD_CLIENT_9_D_A/>)}
+    { category === '9_D_B' && (<DURER_OLD_CLIENT_9_D_B/>)}
+    { category === '9_D_C' && (<DURER_OLD_CLIENT_9_D_C/>)}
+    { category === '9_D_Cp' && (<DURER_OLD_CLIENT_9_D_Cp/>)}
+    { category === '9_D_D' && (<DURER_OLD_CLIENT_9_D_D/>)}
+    { category === '9_D_Dp' && (<DURER_OLD_CLIENT_9_D_Dp/>)}
+    { category === '9_H_A' && (<DURER_OLD_CLIENT_9_H_A/>)}
+    { category === '9_H_B' && (<DURER_OLD_CLIENT_9_H_B/>)}
     { category === '10_D_A' && (<DURER_OLD_CLIENT_10_D_A/>)}
     { category === '10_D_B' && (<DURER_OLD_CLIENT_10_D_B/>)}
     { category === '10_D_C' && (<DURER_OLD_CLIENT_10_D_C/>)}
+    { category === '10_D_Cp' && (<DURER_OLD_CLIENT_10_D_Cp/>)}
+    { category === '10_D_D' && (<DURER_OLD_CLIENT_10_D_D/>)}
+    { category === '10_D_Dp' && (<DURER_OLD_CLIENT_10_D_Dp/>)}
+    { category === '10_H_A' && (<DURER_OLD_CLIENT_10_H_A/>)}
+    { category === '10_H_B' && (<DURER_OLD_CLIENT_10_H_B/>)}
+    { category === '11_D_A' && (<DURER_OLD_CLIENT_11_D_A/>)}
+    { category === '11_D_B' && (<DURER_OLD_CLIENT_11_D_B/>)}
+    { category === '11_D_C' && (<DURER_OLD_CLIENT_11_D_C/>)}
+    { category === '11_D_Cp' && (<DURER_OLD_CLIENT_11_D_Cp/>)}
+    { category === '11_D_D' && (<DURER_OLD_CLIENT_11_D_D/>)}
+    { category === '11_D_Dp' && (<DURER_OLD_CLIENT_11_D_Dp/>)}
+    { category === '11_H_A' && (<DURER_OLD_CLIENT_11_H_A/>)}
+    { category === '11_H_B' && (<DURER_OLD_CLIENT_11_H_B/>)}
+    { category === '12_D_A' && (<DURER_OLD_CLIENT_12_D_A/>)}
+    { category === '12_D_B' && (<DURER_OLD_CLIENT_12_D_B/>)}
+    { category === '12_D_C' && (<DURER_OLD_CLIENT_12_D_C/>)}
+    { category === '12_D_D' && (<DURER_OLD_CLIENT_12_D_D/>)}
+    { category === '12_D_E' && (<DURER_OLD_CLIENT_12_D_E/>)}
+    { category === '12_D_Ep' && (<DURER_OLD_CLIENT_12_D_Ep/>)}
+    { category === '12_H_A' && (<DURER_OLD_CLIENT_12_H_A/>)}
+    { category === '12_H_B' && (<DURER_OLD_CLIENT_12_H_B/>)}
     </>
   );
 }
