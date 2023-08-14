514788961cddf658ad7b77261e2b95d94281f199
OnionKiller <tomi@csilling.com>
2023-08-14 20:07:02 +0200

    reafctor: move mach related functionalitites to a separated endpoint

--- src/server/router.ts
+++ src/server/router.ts
@@ -20,7 +20,7 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
    * @param {string} matchId - The ID of the match.
    * @returns {LogEntry[]} - A list of log objects.
    */
-  router.get('/team/admin/:matchId/logs', async (ctx) => {
+  router.get('/match/admin/:matchId/logs', async (ctx) => {
     //It is already authenticated by the admin mount routing
     const matchID = ctx.params.matchId;
     const { log } = await (ctx.db as StorageAPI.Async).fetch(matchID, {
@@ -38,7 +38,7 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
    * @param {string} matchId - The ID of the match.
    * @returns {State<any>} - A match state object object.
    */
-  router.get('/team/admin/:matchId/state', async (ctx) => {
+  router.get('/match/admin/:matchId/state', async (ctx) => {
     const matchID = ctx.params.matchId;
     const { state } = await (ctx.db as StorageAPI.Async).fetch(matchID, {
       state: true,
@@ -55,7 +55,7 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
    * @param {string} matchId - The ID of the match.
    * @returns {Server.MatchData} - A match object.
    */
-     router.get('/team/admin/:matchId/metadata', async (ctx) => {
+     router.get('/match/admin/:matchId/metadata', async (ctx) => {
       const matchID = ctx.params.matchId;
       const { metadata } = await (ctx.db as StorageAPI.Async).fetch(matchID, {
         metadata: true,
