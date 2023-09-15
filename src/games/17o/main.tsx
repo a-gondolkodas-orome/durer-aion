108c5b6c03f35e8927da900842bac9a5d3a9a484
Jánosik Áron <aron@janosik.hu>
2023-09-15 18:05:47 +0200

    fix: game description added

--- src/games/17o/main.tsx
+++ src/games/17o/main.tsx
@@ -5,15 +5,15 @@ import { ClientFactory } from '../../common/client_factory';
 
 
 let descriptionC = <p className="text-justify">
-Kezdetben van 10 érme az asztalon, melyek értékei 1 és 4 közé eső egészek lehetnek. A két játékos felváltva lép. A soron lévő játékos kiválaszt egy K értéket, amire igaz, hogy van az asztalon K értékű érme, és az összes K értékű érmét átváltoztatja valamilyen kisebb L értékűre (mindet ugyanarra az L értékre, ahol az L érték 1 és K-1 közötti). Az nyer, akinek a lépése után minden érme azonos értékű lesz. A kezdőállás ismeretében Ti dönthetitek el, hogy a kezdő vagy a második játékos bőrébe szeretnétek-e bújni.
+Kezdetben van egy kupac zseton az asztalon. A két játékos felváltva lép. A soron lévő játékos elvehet egy zsetont a kupacból, vagy ha páros számú zseton van az asztalon, akkor elveheti a zsetonok felét. Az nyer, akinek a lépése után nem marad zseton az asztalon. A kezdőállás ismeretében Ti dönthetitek el, hogy a kezdő vagy a második játékos bőrébe szeretnétek-e bújni.
 
-(K: Melyik típusú érmét változtatjátok meg?; L: Mi legyen az új érme)
+
 </p>
 
 let descriptionDE = <p className="text-justify">
-Kezdetben van 10 érme az asztalon, melyek értékei 1 és 5 közé eső egészek lehetnek. A két játékos felváltva lép. A soron lévő játékos kiválaszt egy K értéket, amire igaz, hogy van az asztalon K értékű érme, és az összes K értékű érmét átváltoztatja valamilyen kisebb L értékűre (mindet ugyanarra az L értékre, ahol az L érték 1 és K-1 közötti). Az nyer, akinek a lépése után minden érme azonos értékű lesz. A kezdőállás ismeretében Ti dönthetitek el, hogy a kezdő vagy a második játékos bőrébe szeretnétek-e bújni.
+Kezdetben van egy kupac zseton az asztalon. A két játékos felváltva lép. A soron lévő játékos elvehet egy zsetont a kupacból, vagy ha páros számú zseton van az asztalon, akkor elveheti a zsetonok felét. Az nyer, akinek a lépése után nem marad zseton az asztalon. A kezdőállás ismeretében Ti dönthetitek el, hogy a kezdő vagy a második játékos bőrébe szeretnétek-e bújni.
 
-(K: Melyik típusú érmét változtatjátok meg?; L: Mi legyen az új érme)
+
 </p>
 
 export const { Client: Client_C, ClientWithBot: ClientWithBot_C, OnlineClient: OnlineClient_C } = ClientFactory({...MyGame, name: "tencoins_c"}, MyBoard, strategyWrapper("C"), descriptionC);
