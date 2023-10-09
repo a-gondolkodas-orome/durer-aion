a6a21ca5705e0ba21dd0db1683c10c22bea3861f
Ádám Fraknói <fraknoiadam@gmail.com>
2023-10-09 20:29:29 +0200

    feat: update sendData

--- src/common/sendData.ts
+++ src/common/sendData.ts
@@ -1,19 +1,29 @@
 import { TeamModelDto } from "../client/dto/TeamStateDto";
 
 function sendData(fileName: string, data: string){
+  return;
   const fd = new FormData();
-  fd.append('key', fileName);
+  fd.append('key', "titok/"+fileName);
   fd.append('file', data);
-  fetch('https://durer-d4ee5dcab098f3d6755c03070e1a71bf.s3.eu-central-1.amazonaws.com/', { method: 'POST', body: fd, mode: 'cors'}).then(res => console.log(res.status));
+  // utf8 charset
+  fd.append('Content-Type', 'text/plain; charset=utf-8');
+  fetch('https://adamprobalkozik.s3.eu-central-1.amazonaws.com', { method: 'POST', body: fd, mode: 'cors'}).then(res => console.log(res.status));
 }
 
-export function sendDataStart(teamState: TeamModelDto | null){
+const randomID = Math.floor(Math.random() * 900000)+100000;
+
+function now(){
+  const date = new Date()
+  const result = date.toISOString().replace(/[^A-Za-z0-9]+/g,'').slice(0, -1)
+  return result;
+}
+
+export function sendDataLogin(teamState: TeamModelDto | null){
   let code = "null";
   if (teamState !== null) {
     code = teamState.joinCode;
   }
-  let now = new Date().toISOString();
-  sendData(code+"_start_"+now, "code");
+  sendData(code+"_"+randomID+"_login_"+now(), "code");
 }
 
 
@@ -22,19 +32,16 @@ export function sendDataRelayStart(teamState: TeamModelDto | null){
   if (teamState !== null) {
     code = teamState.joinCode;
   }
-  let now = new Date().toISOString();
-  sendData(code+"_relaystart_"+now, "");
+  sendData(code+"_"+randomID+"_relaystart_"+now(), "");
 }
 
-export function sendDataRelayStep(teamState: TeamModelDto | null, G: any, ctx: any){
+export function sendDataRelayStep(teamState: TeamModelDto | null, G: any, ctx: any, answer: number){
   let code = "null";
   if (teamState !== null) {
     code = teamState.joinCode;
   }
-  let now = new Date().toISOString();
   let problemNumber = G.currentProblem;
-  let answer = G.answer;
-  sendData(code+"_relay_"+problemNumber+"_"+answer+"_"+now, JSON.stringify(G)+JSON.stringify(ctx));
+  sendData(code+"_"+randomID+"_relay_"+problemNumber+"_"+answer+"_"+now(), JSON.stringify({G, ctx}));
 }
 
 export function sendDataRelayEnd(teamState: TeamModelDto | null, G: any, ctx: any){
@@ -42,9 +49,8 @@ export function sendDataRelayEnd(teamState: TeamModelDto | null, G: any, ctx: an
   if (teamState !== null) {
     code = teamState.joinCode;
   }
-  let now = new Date().toISOString();
   let points = G.points;
-  sendData(code+"_relayend_"+points+"_"+now, JSON.stringify(G)+JSON.stringify(ctx));
+  sendData(code+"_"+randomID+"_relayend_"+points+"_"+now(), JSON.stringify({G, ctx}));
 }
 
 export function sendDataStrategyStart(teamState: TeamModelDto | null){
@@ -52,8 +58,7 @@ export function sendDataStrategyStart(teamState: TeamModelDto | null){
   if (teamState !== null) {
     code = teamState.joinCode;
   }
-  let now = new Date().toISOString();
-  sendData(code+"_stratstart_"+now, "");
+  sendData(code+"_"+randomID+"_stratstart_"+now(), "");
 }
 
 export function sendDataStrategyStep(teamState: TeamModelDto | null, K:number, L:number,  G: any, ctx: any){
@@ -61,8 +66,7 @@ export function sendDataStrategyStep(teamState: TeamModelDto | null, K:number, L
   if (teamState !== null) {
     code = teamState.joinCode;
   }
-  let now = new Date().toISOString();
-  sendData(code+"_strat_"+K+L+"_"+now, JSON.stringify(G)+JSON.stringify(ctx));
+  sendData(code+"_"+randomID+"_strat_"+K+L+"_"+now(), JSON.stringify({G, ctx}));
 }
 
 export function sendDataStrategyEnd(teamState: TeamModelDto | null, G: any, ctx: any){
@@ -70,7 +74,6 @@ export function sendDataStrategyEnd(teamState: TeamModelDto | null, G: any, ctx:
   if (teamState !== null) {
     code = teamState.joinCode;
   }
-  let now = new Date().toISOString();
   let points = G.points;
-  sendData(code+"_stratend_"+points+"_"+now, JSON.stringify(G)+JSON.stringify(ctx));
+  sendData(code+"_"+randomID+"_stratend_"+points+"_"+now(), JSON.stringify({G, ctx}));
 }
