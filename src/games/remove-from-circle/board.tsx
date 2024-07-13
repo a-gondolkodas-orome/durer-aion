c0ea0bdede772b32d59eef4f1b1e00652795b3fe
HajosB144 <hbalazs.mat@gmail.com>
2024-07-13 21:02:36 +0200

    Restructuring data storage to uniformize first move

--- src/games/remove-from-circle/board.tsx
+++ src/games/remove-from-circle/board.tsx
@@ -1,7 +1,5 @@
 import { MyGameState } from './game';
 import { BoardProps } from 'boardgame.io/react';
-import Stack from '@mui/material/Stack';
-import { Button } from '@mui/material';
 import { sendDataStrategyStep } from '../../common/sendData';
 import { range } from "lodash";
 
@@ -17,16 +15,19 @@ export function MyBoard({ G, ctx, moves }: MyGameProps) {
     moves.removePoint(index)
   };
 
-  
+  const getAngle = (index: number) => {
+    let step = Math.PI*2/G.circle.length;
+    return index*step + (G.firstMove === -1 ? 0 : G.firstMove*step)
+  };
 
-  return (<>
+  return (
     <svg>
       {
         range(G.circle.length).map(index => (
           <line
           key={`${index-1}-${index}`}
-          x1={55+50*Math.cos(index*Math.PI*2/G.circle.length)} y1={55+50*Math.sin(index*Math.PI*2/G.circle.length)}
-          x2={55+50*Math.cos((index-1)*Math.PI*2/G.circle.length)} y2={55+50*Math.sin((index-1)*Math.PI*2/G.circle.length)}
+          x1={55+50*Math.cos(getAngle(index))} y1={55+50*Math.sin(getAngle(index))}
+          x2={55+50*Math.cos(getAngle(index-1))} y2={55+50*Math.sin(getAngle(index-1))}
           stroke = {(G.circle.at(index-1) && G.circle.at(index)) ? "black" : "gray"}
           strokeWidth={4}
           />
@@ -35,13 +36,12 @@ export function MyBoard({ G, ctx, moves }: MyGameProps) {
         range(G.circle.length).map(index => (
           <circle 
             key={index}
-            cx={55+50*Math.cos(index*Math.PI*2/G.circle.length)} cy={55+50*Math.sin(index*Math.PI*2/G.circle.length)}
+            cx={55+50*Math.cos(getAngle(index))} cy={55+50*Math.sin(getAngle(index))}
             r="2%" fill={G.circle[index] && (G.circle.at(index-1) || G.circle[(index+1)%G.circle.length]) ? "black" : "gray"}
             onClick={() => onClick(index)}
           /> 
         ))}
     </svg>
-    </>
   )
 }
 
