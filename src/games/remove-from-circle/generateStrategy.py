4d739c26b23f0ea56dcafe06c1c783b517ee8428
HajosB144 <hbalazs.mat@gmail.com>
2024-07-14 14:27:53 +0200

    Python script for startegy

---
+++ src/games/remove-from-circle/generateStrategy.py
@@ -0,0 +1,44 @@
+n = 15
+
+def isEnd(l):
+  for i in l:
+    if l != 0 and l != -1:
+      return False
+  return True
+
+moves = [list(map(int, list(bin(2**(n-1)+i)[3:]))) for i in range(2**(n-1))]
+
+for i in range(len(moves)):
+  if moves[i][0] != 0 and moves[i][1] != 0:
+    moves[i][0] = i-2**(n-2)
+  elif moves[i][0] != 0:
+    moves[i][0] = -1
+  if moves[i][-1] != 0 and moves[i][-2] != 0:
+    moves[i][-1] = i-1
+  elif moves[i][-1] != 0:
+    moves[i][-1] = -1
+  for j in range(2, n-1):
+    if moves[i][-j] != 0 and (moves[i][(2*n-2-j-1)%(n-1)] or moves[i][(2*n-2-j+1)%(n-1)]):
+      moves[i][-j] = i-2**(j-1)
+    elif moves[i][-j] != 0:
+      moves[i][-j] = -1  
+
+winners = []
+steps = []
+for i in range(len(moves)):
+  if isEnd(moves[i]):
+    winners.append(2)
+    continue
+  for j in range(len(moves[i])):
+    if moves[i][j] == 0 or moves[i][j] == -1:
+      pass
+    else:
+      if winners[moves[i][j]] == 2:
+        winners.append(1)
+        steps.append(j)
+        break
+  else:
+    winners.append(2)
+    steps.append(None)
+
+print(steps)
