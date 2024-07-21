940e805ef9bc41a05ef93e72e1af46ff1c8cff74
HajosB144 <hbalazs.mat@gmail.com>
2024-07-21 18:09:40 +0200

    Updated board display

--- src/games/remove-from-circle/board.tsx
+++ src/games/remove-from-circle/board.tsx
@@ -21,26 +21,39 @@ export function MyBoard({ G, ctx, moves }: MyGameProps) {
   };
 
   return (
-    <svg>
+    <svg viewBox='0 0 200 200'>
       {
         range(G.circle.length).map(index => (
           <line
           key={`${index-1}-${index}`}
           x1={55+50*Math.cos(getAngle(index))} y1={55+50*Math.sin(getAngle(index))}
           x2={55+50*Math.cos(getAngle(index-1))} y2={55+50*Math.sin(getAngle(index-1))}
-          stroke = {(G.circle.at(index-1) && G.circle.at(index)) ? "black" : "gray"}
-          strokeWidth={4}
+          stroke = {(G.circle.at(index-1) && G.circle.at(index)) ? "green" : "red"}
+          strokeWidth={2}
           />
         ))}
-      {
+      {/*
         range(G.circle.length).map(index => (
           <circle 
             key={index}
             cx={55+50*Math.cos(getAngle(index))} cy={55+50*Math.sin(getAngle(index))}
-            r="2%" fill={G.circle[index] && (G.circle.at(index-1) || G.circle[(index+1)%G.circle.length]) ? "black" : "gray"}
+            r="2%" fill={G.circle[index] && (G.circle.at(index-1) || G.circle[(index+1)%G.circle.length]) ? "green" : "red"}
             onClick={() => onClick(index)}
           /> 
-        ))}
+        ))*/}
+        {
+    range(G.circle.length).map(index => (
+      <circle 
+        key={index}
+        cx={55+50*Math.cos(getAngle(index))} cy={55+50*Math.sin(getAngle(index))}
+        r="2%"
+        fill={index === G.lastMove ? "white" : (G.circle[index] && (G.circle.at(index-1) || G.circle[(index+1)%G.circle.length]) ? "green" : "red")}
+        stroke={index === G.lastMove ? "red" : "none"}
+        strokeWidth={index === G.lastMove ? "1%" : "0"}
+        onClick={() => onClick(index)}
+      /> 
+    ))
+  }
     </svg>
   )
 }
--- src/games/remove-from-circle/generateStrategy.py
+++ src/games/remove-from-circle/generateStrategy.py
@@ -1,4 +1,4 @@
-n = 15
+n = 10
 
 def isEnd(l):
   for i in l:
@@ -23,22 +23,23 @@ for i in range(len(moves)):
     elif moves[i][-j] != 0:
       moves[i][-j] = -1  
 
-winners = []
-steps = []
+winners = [2]*len(moves)
+steps = [[] for i in range(len(moves))]
+
 for i in range(len(moves)):
-  if isEnd(moves[i]):
-    winners.append(2)
-    continue
   for j in range(len(moves[i])):
     if moves[i][j] == 0 or moves[i][j] == -1:
       pass
     else:
       if winners[moves[i][j]] == 2:
-        winners.append(1)
-        steps.append(j)
-        break
-  else:
-    winners.append(2)
-    steps.append(None)
+        winners[i] = 1
+        steps[i].append(j)
 
-print(steps)
+#%%
+d = {bin(i+2**(n-1))[3:]:steps[i] for i in range(len(moves))}
+#print(d)
+
+# %%
+for key in d.keys():
+  if key.count('0') == 2 and key[4]=='0':
+    print(key, d[key])
