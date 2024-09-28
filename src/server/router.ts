d10272097ce7aef2ba2879ce5c3e81e8dbe16c97
OnionKiller <tomi@csilling.com>
2024-09-28 10:48:41 +0200

    chore: fomratting

--- src/server/router.ts
+++ src/server/router.ts
@@ -52,70 +52,74 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
     ctx.body = state;
   });
 
-    /**
-   * Add extra timeto a specific match.
-   *
-   * @param {string} matchId - The ID of the match.
-   * @param {integer} minutes - How many minutes to add
-   * @returns {State<any>} - A match state object object.
-   */
+  /**
+ * Add extra timeto a specific match.
+ *
+ * @param {string} matchId - The ID of the match.
+ * @param {integer} minutes - How many minutes to add
+ * @returns {State<any>} - A match state object object.
+ */
   router.get('/game/admin/:matchId/addminutes/:minutes', async (ctx) => {
-      const matchID = ctx.params.matchId;
-      const minutes = Number(ctx.params.minutes);
-      const { state, metadata } = await (ctx.db as StorageAPI.Async).fetch(matchID, {
-        state: true,
-        metadata: true
-      });
-      if (!state) {
-        ctx.throw(404, 'Match ' + matchID + ' not found');
-      }
-      // Fetch team
-      const teamId = metadata.players[0].name;
-      const team = await teams.getTeam({teamId})
+    const matchID = ctx.params.matchId;
+    const minutes = Number(ctx.params.minutes);
+    const { state, metadata } = await (ctx.db as StorageAPI.Async).fetch(matchID, {
+      state: true,
+      metadata: true
+    });
+    if (!state) {
+      ctx.throw(404, 'Match ' + matchID + ' not found');
+    }
+    // Fetch team
+    const teamId = metadata.players[0].name;
+    const team = await teams.getTeam({ teamId })
 
-      if (!team) {
-        ctx.throw(500, `Match found, but assigned team ${teamId} was not found.`);
-        return
-      }
+    if (!team) {
+      ctx.throw(500, `Match found, but assigned team ${teamId} was not found.`);
+      return
+    }
 
-      const upDate =  new Date(state.G.end)
-      upDate.setMinutes(upDate.getMinutes() + minutes)
+    const upDate = new Date(state.G.end)
+    upDate.setMinutes(upDate.getMinutes() + minutes)
 
-      //Update state
-      state.G.end = upDate.toISOString();
-      const upd = state.G.end;
-      state.G.milisecondsRemaining = upDate.getTime() - new Date().getTime();
-      //manually increment stateID
-      //really hope this doesn't breaky anything
-      state._stateID += 1
+    //Update state
+    state.G.end = upDate.toISOString();
+    const upd = state.G.end;
+    state.G.milisecondsRemaining = upDate.getTime() - new Date().getTime();
+    //manually increment stateID
+    //really hope this doesn't breaky anything
+    state._stateID += 1
 
-      //Update team
-      //TODO: review super duper forced update alternatives
-      if(team.strategyMatch.state == "IN PROGRESS")
-        await team.update({strategyMatch:{
+    //Update team
+    //TODO: review super duper forced update alternatives
+    if (team.strategyMatch.state == "IN PROGRESS")
+      await team.update({
+        strategyMatch: {
           state: 'IN PROGRESS',
           matchID: matchID,
           startAt: new Date(state.G.start),
           endAt: upDate,
-        }})
-      else if(team.relayMatch.state == "IN PROGRESS")
-      await team.update({relayMatch:{
-        state: 'IN PROGRESS',
-        matchID: matchID,
-        startAt: new Date(state.G.start),
-        endAt: upDate,
-      }})
-      else
+        }
+      })
+    else if (team.relayMatch.state == "IN PROGRESS")
+      await team.update({
+        relayMatch: {
+          state: 'IN PROGRESS',
+          matchID: matchID,
+          startAt: new Date(state.G.start),
+          endAt: upDate,
+        }
+      })
+    else
       ctx.throw(501, 'Restarting an already finished match is not supported right now.');
-    
-    
-    await ctx.db.setState(matchID,state);
+
+
+    await ctx.db.setState(matchID, state);
 
     team.other += ` te[${matchID}]:${minutes}`
     team.save()
-    
-    ctx.body = {updatedEndTime:upDate,matchID:matchID, team:team};
-    
+
+    ctx.body = { updatedEndTime: upDate, matchID: matchID, team: team };
+
   });
 
   /**
@@ -125,15 +129,15 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
    * @returns {Server.MatchData} - A match object.
    */
   router.get('/game/admin/:matchId/metadata', async (ctx) => {
-      const matchID = ctx.params.matchId;
-      const { metadata } = await (ctx.db as StorageAPI.Async).fetch(matchID, {
-        metadata: true,
-      });
-      if (!metadata) {
-        ctx.throw(404, 'Match ' + matchID + ' not found');
-      }
-      ctx.body = metadata;
+    const matchID = ctx.params.matchId;
+    const { metadata } = await (ctx.db as StorageAPI.Async).fetch(matchID, {
+      metadata: true,
     });
+    if (!metadata) {
+      ctx.throw(404, 'Match ' + matchID + ' not found');
+    }
+    ctx.body = metadata;
+  });
 
   /**
      * Reset teams's strategy
@@ -143,20 +147,20 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
      */
   router.get('/team/admin/:teamID/reset/strategy', async (ctx) => {
     const teamId = ctx.params.teamID;
-    const team = await teams.getTeam({teamId})
+    const team = await teams.getTeam({ teamId })
 
     if (!team) {
       ctx.throw(404, `Team not found with teamID ${teamId}.`);
       return
     }
     // reset the strategy game while playing
-    if(team.pageState == 'STRATEGY')
+    if (team.pageState == 'STRATEGY')
       team.pageState = 'HOME'
 
     //log earlier matchid
-    if(team.strategyMatch.state != 'NOT STARTED')
+    if (team.strategyMatch.state != 'NOT STARTED')
       team.other += ` prevstratid:${team.strategyMatch.matchID}`
-    team.strategyMatch = {state:'NOT STARTED'}
+    team.strategyMatch = { state: 'NOT STARTED' }
     team.save();
     ctx.body = team
   });
@@ -169,20 +173,20 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
      */
   router.get('/team/admin/:teamID/reset/relay', async (ctx) => {
     const teamId = ctx.params.teamID;
-    const team = await teams.getTeam({teamId})
+    const team = await teams.getTeam({ teamId })
 
     if (!team) {
       ctx.throw(404, `Team not found with teamID ${teamId}.`);
       return
     }
     // reset the strategy game while playing
-    if(team.pageState == 'RELAY')
+    if (team.pageState == 'RELAY')
       team.pageState = 'HOME'
 
     //log earlier matchid
-    if(team.relayMatch.state != 'NOT STARTED')
+    if (team.relayMatch.state != 'NOT STARTED')
       team.other += ` prevrelayid:${team.relayMatch.matchID}`
-    team.relayMatch = {state:'NOT STARTED'}
+    team.relayMatch = { state: 'NOT STARTED' }
     team.save();
     ctx.body = team
   });
@@ -194,14 +198,14 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
    * @returns {TeamModel[]} - List of the selected teams 
    */
   router.get('/team/admin/filter', koaBody(), async (ctx) => {
-    const filter_string:string|string[]|undefined = ctx.request.query['filter'];
-    let filters:string[];
+    const filter_string: string | string[] | undefined = ctx.request.query['filter'];
+    let filters: string[];
     if (filter_string === undefined) {
       filters = []
-    } else if (typeof(filter_string) === 'string' ) {
+    } else if (typeof (filter_string) === 'string') {
       filters = filter_string.split(',');
     }
-    else{
+    else {
       filters = filter_string;
     }
     //TODO: fix the return type value
@@ -213,7 +217,7 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
    * Get all teams as a full object
    * @returns {TeamModel[]} - List of the selected teams 
    */
-  router.get('/team/admin/all',koaBody(),async (ctx) => {
+  router.get('/team/admin/all', koaBody(), async (ctx) => {
     ctx.body = await teams.listTeams();
   })
 
@@ -245,7 +249,7 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
     console.log(GUID);
     let team = await teams.getTeam({ teamId: GUID }) ?? ctx.throw(404, `Team with {teamId:${GUID}} not found.`)
     const staleInfo = await checkStaleMatch(team);
-    if (staleInfo.isStale){
+    if (staleInfo.isStale) {
       console.log(`Stale found: ${JSON.stringify(staleInfo)}`)
       await closeMatch((team[staleInfo.gameState!] as InProgressMatchStatus).matchID, teams, ctx.db);
       team = await teams.getTeam({ teamId: GUID }) ?? ctx.throw(404, `Team with {teamId:${GUID}} not found.`)
@@ -269,12 +273,12 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
 
     // about to start a game
     const body: LobbyAPI.CreatedMatch = await createGame(game, ctx);
-    await injectPlayer(ctx.db, body.matchID, {playerID: '0', name: GUID, credentials: team.credentials});
+    await injectPlayer(ctx.db, body.matchID, { playerID: '0', name: GUID, credentials: team.credentials });
     await injectBot(ctx.db, body.matchID, BOT_ID);
 
     //created new game, updated team state accordingly
     const match = await startMatchStatus(body.matchID, ctx);
-    if(match.startAt === null  || match.endAt === null){
+    if (match.startAt === null || match.endAt === null) {
       console.error(`GAME [${game.name}] initialiser doesn't initialise the timer!!!`)
     }
     team.update({
@@ -297,10 +301,10 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
     //about to start
 
     const body: LobbyAPI.CreatedMatch = await createGame(game, ctx);
-    await injectPlayer(ctx.db, body.matchID, {playerID: '0', name: GUID, credentials: team.credentials});
+    await injectPlayer(ctx.db, body.matchID, { playerID: '0', name: GUID, credentials: team.credentials });
     await injectBot(ctx.db, body.matchID, BOT_ID);
 
-   //created new game, updated team state accordingly
+    //created new game, updated team state accordingly
     team.update({
       pageState: 'STRATEGY',
       strategyMatch: await startMatchStatus(body.matchID, ctx)
@@ -320,7 +324,7 @@ export function configureTeamsRouter(router: Router<any, Server.AppCtx>, teams: 
     const team: TeamModel = await teams.getTeam({ teamId: GUID }) ?? ctx.throw(404, `Team with {id:${GUID}} not found.`)
     if (team.relayMatch.state == 'IN PROGRESS' || team.strategyMatch.state == 'IN PROGRESS')
       ctx.throw(403, "Not allowed, match in progress.")
-    
+
     //update team state to go home
     team.update({
       pageState: 'HOME',
