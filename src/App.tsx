8dacbb4c25ce163341047db2d43f802f92f3907e
HajosB144 <hbalazs.mat@gmail.com>
2024-10-02 19:35:15 +0200

    Revert App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import { Main } from './client/components/Main';
-import {ClientWithBot_E} from './games/remove-from-circle/main';
 
 function App() {
 
   return (
-    <ClientWithBot_E />
+    <Main />
   )
 }
 
