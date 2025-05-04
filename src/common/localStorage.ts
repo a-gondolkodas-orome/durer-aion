8df88dbd7c61147d05bb40e2a97ec2570eaa9d22
Ádám Fraknói <fraknoiadam@gmail.com>
2025-05-04 12:02:58 +0200

    some refactors

--- src/common/localStorage.ts
+++ src/common/localStorage.ts
@@ -1,70 +1,94 @@
-import { Game } from "boardgame.io";
-import { IS_OFFLINE_MODE } from "../client/utils/util";
-import { GameStateTimer, GUESSER_PLAYER } from "./types";
+import { Ctx, Game } from "boardgame.io";
 
-function parseGameState<T_SpecificGameState>(json: string): T_SpecificGameState {
+import { GameStateTimer, GUESSER_PLAYER } from "./types";
+import { IS_OFFLINE_MODE } from "../client/utils/util";
+
+
+function parseJSON<T_ParsedData>(json: string): T_ParsedData {
   const parsed = JSON.parse(json);
   if (typeof parsed !== 'object' || parsed === null) {
-    throw new Error('strategy: game_state_from_json: Invalid JSON: not an object');
+    throw new Error(`Invalid JSON: not an object. Received: ${json}`);
   }
-  return parsed as T_SpecificGameState;
+  return parsed as T_ParsedData;
 }
+
 
 function saveGameState({ G, ctx }: any, gameName: string) {
   if (!IS_OFFLINE_MODE) {
     return;
   }
   localStorage.setItem(gameName + "GameState", JSON.stringify(G));
-  localStorage.setItem(gameName + "Phase", ctx.phase);
+  localStorage.setItem(gameName + "ctx", JSON.stringify(ctx));
 }
 
-function loadGameStateFromLocalStorage<T_SpecificGameState>(gameName: string): { savedG?: T_SpecificGameState & GameStateTimer, savedPhase?: string } {
-  const savedPhase = localStorage.getItem(gameName + "Phase");
+
+function loadGameStateFromLocalStorage<T_SpecificGameState>(gameName: string): { savedG: T_SpecificGameState & GameStateTimer, savedctx: Ctx} | undefined {
+  const ctxJSON = localStorage.getItem(gameName + "ctx");
   const gameStateJSON = localStorage.getItem(gameName + "GameState");
-  if (savedPhase === null || gameStateJSON === null) {
-    return {};
+  if (ctxJSON === null || gameStateJSON === null) {
+    return;
   }
 
-  let savedG;
+  let savedG, savedctx;
   try { // if the json is bad, continue as if we didnt even have it
-    savedG = parseGameState<T_SpecificGameState & GameStateTimer>(gameStateJSON);
-  } catch {
-    localStorage.removeItem(gameName + "Phase");;
-    localStorage.removeItem(gameName + "GameState");;
-    console.error("could not load game phase from json, invalid json");
-    return {};
+    savedG = parseJSON<T_SpecificGameState & GameStateTimer>(gameStateJSON);
+    savedctx = parseJSON<Ctx>(ctxJSON);
+  } catch (error) {
+    localStorage.removeItem(gameName + "ctx");
+    localStorage.removeItem(gameName + "GameState");
+    console.error(error);
+    return;
   }
 
   savedG.millisecondsRemaining = new Date(savedG.end).getTime() - new Date().getTime();
-  return { savedG, savedPhase };
+  return { savedG, savedctx };
 }
+
+
+function injectFuncOnBeginTurn(myGameWrapper: any, newFunction: any) {
+  for (const phaseName in myGameWrapper.phases) {
+    const originalOnBeginTurn = myGameWrapper.phases[phaseName].turn?.onBegin;
+    myGameWrapper.phases[phaseName] = {
+      ...myGameWrapper.phases[phaseName],
+      turn: {
+        ...myGameWrapper.phases[phaseName].turn,
+        onBegin: (state: any) => {
+          if (originalOnBeginTurn) {
+            originalOnBeginTurn(state);
+          }
+          newFunction(state);
+        },
+      },
+    }
+  }
+  return myGameWrapper;
+}
+
 
 export function setupLocalStorageGameState<T_SpecificGameState>(myGameWrapper: Game<T_SpecificGameState & GameStateTimer>) {
   const gameName = myGameWrapper.name;
   if (!gameName) {
-    throw new Error("Gamename is not defined. Local storage game state save is not possible.");
+    throw new Error("'name' is not defined in the game. LocalStorage save is not supported without game name.");
   }
-  const { savedG, savedPhase } = loadGameStateFromLocalStorage(gameName);
+  if (!myGameWrapper.phases) {
+    throw new Error("Limitation: LocalStorage save is not supported in games without phases.");
+  }
 
   // Save the game state to localStorage for onBegin guesser's turn
-  for (const phaseName in myGameWrapper.phases) {
-    const originalOnBeginTurn = myGameWrapper.phases[phaseName].turn?.onBegin;
-    myGameWrapper.phases[phaseName].turn = {
-      ...myGameWrapper.phases[phaseName].turn,
-      onBegin: (state) => {
-        if (originalOnBeginTurn) {
-          originalOnBeginTurn(state);
-        }
-        if (state.ctx.currentPlayer === GUESSER_PLAYER) {
-          saveGameState(state, gameName);
-        }
-      },
+  myGameWrapper = injectFuncOnBeginTurn(myGameWrapper, (state: any) => {
+    if (state.ctx.currentPlayer === GUESSER_PLAYER) {
+      saveGameState(state, gameName);
     }
-  }
+  });
 
-  if (!myGameWrapper.phases || !savedPhase) {
+  // Save the game state to localStorage for onEnd of game
+  // NOT FINISHED THIS PART
+
+  const savedStates = loadGameStateFromLocalStorage<T_SpecificGameState>(gameName);
+  if (!myGameWrapper.phases || !savedStates) {
     return myGameWrapper; // No loaded state.
   }
+  const { savedG, savedctx } = savedStates;
 
   // Initialize game state with the loaded one.
   const originalSetup = myGameWrapper.setup;
@@ -79,13 +103,13 @@ export function setupLocalStorageGameState<T_SpecificGameState>(myGameWrapper: G
       myGameWrapper.phases[phaseName].start = false;
     }
   }
-  myGameWrapper.phases[savedPhase].start = true;
-
+  myGameWrapper.phases[savedctx.phase].start = true;
+  
   // Guesser player starts the turn (even if the phase turn order specifies otherwise)
-  const originalTurnOrder = myGameWrapper.phases[savedPhase].turn;
+  const originalTurnOrder = myGameWrapper.phases[savedctx.phase].turn;
   let forceFirstPlayer = true;
   if (originalTurnOrder?.order?.first) {
-    myGameWrapper.phases[savedPhase].turn = {
+    myGameWrapper.phases[savedctx.phase].turn = {
       ...originalTurnOrder,
       order: {
         first: (state) => {
@@ -101,5 +125,13 @@ export function setupLocalStorageGameState<T_SpecificGameState>(myGameWrapper: G
       }
     }
   }
+
+  // End game if the game ended in the saved state
+  if (savedctx.gameover) {
+    myGameWrapper = injectFuncOnBeginTurn(myGameWrapper, (state: any) => {
+      state.events.endGame();
+    });
+  }
+  
   return myGameWrapper;
 }
