1fe79ccae813c02f316342f3cc0382c84cf4375d
david <david.roman@proglimes.com>
2025-09-05 20:06:07 +0200

    add dev:offline

--- apps/offline-frontend/vite.config.ts
+++ apps/offline-frontend/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      game: path.resolve(__dirname, "../../packages/game"),
+      schemas: path.resolve(__dirname, "../../packages/schemas"),
+      strategy: path.resolve(__dirname, "../../packages/strategy"),
+      "common-frontend": path.resolve(__dirname, "../../packages/common-frontend"),
+    },
+    preserveSymlinks: true, // this is needed to make sure that linked packages are properly resolved (like game and schemas
+  },
+  server: {
+    fs: {
+      allow: [
+        "..", // allow Vite to serve files outside project root
+      ],
+    },
+  },
+  optimizeDeps: {
+    exclude: ["game", "schemas", "strategy", "common-frontend"],
+  },
 })
