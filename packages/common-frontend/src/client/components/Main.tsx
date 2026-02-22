f2ca44a11f57f928a18fc2e6a1f330de92711603
OregSamSas <101597941+OregSamSas@users.noreply.github.com>
2026-02-22 18:08:15 +0100

    remove unnecessary question marks from code

--- packages/common-frontend/src/client/components/Main.tsx
+++ packages/common-frontend/src/client/components/Main.tsx
@@ -49,7 +49,7 @@ export function Main() {
         {admin && <Admin teamId={window.location.pathname.split('/').at(2)}/>}
         {!teamState && !admin && <Login />}
         {teamState && teamState.pageState === "DISCLAIMER" && (
-          <Disclaimer teamName={teamState?.teamName ?? null} category={teamState?.category ?? null}/>
+          <Disclaimer teamName={teamState.teamName} category={teamState.category}/>
         )}
         {teamState && teamState.pageState === "HOME" && frontendState === null && (
           <Chooser state={teamState} setState={setFrontEndState}/>
