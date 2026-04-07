1cf9d82bc3b714cde5140a0542dccda8352edfb3
OregSamSas <101597941+OregSamSas@users.noreply.github.com>
2026-04-07 18:07:30 +0200

    migrate type fixes from #42  (OnionKiller's code) + comment out missing eslint rule + enable ci failure on eslint errors

--- packages/common-frontend/src/client/hooks/user-hooks.ts
+++ packages/common-frontend/src/client/hooks/user-hooks.ts
@@ -30,7 +30,7 @@ export const LoadTeamState = () => {
 
       
           userModel.addListener(setTeamState);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return null;
