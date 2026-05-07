38c35811dc11e9bfc12326f289d87a569a502742
OregSamSas <101597941+OregSamSas@users.noreply.github.com>
2026-05-07 12:13:56 +0200

    forgot to remove languagedetector from i18n config

--- packages/common-frontend/src/common/i18n.ts
+++ packages/common-frontend/src/common/i18n.ts
@@ -1,12 +1,10 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Languagedetector from 'i18next-browser-languagedetector';
 import enTranslation from '../../public/locales/en/translation.json';
 import huTranslation from '../../public/locales/hu/translation.json';
 
 
 i18next
-  .use(Languagedetector)
   .use(initReactI18next)
   .init({
     debug: process.env.NODE_ENV === 'development',
