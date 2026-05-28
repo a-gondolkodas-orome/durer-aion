66efe129c939e73ec7f31461e59e50e4c4d4c446
Turu Tamás <tamas@turu.hu>
2026-05-29 01:00:03 +0200

    add check to end time and keys to sample .env

--- apps/online-backend/src/server/common.ts
+++ apps/online-backend/src/server/common.ts
@@ -14,9 +14,14 @@ export function getGameStartAndEndTime() {
   if (!env.GAME_GLOBAL_END_T) {
     throw new Error('No GAME_GLOBAL_END_T supplied! Do set it in the environment');
   }
+  const globalStartAt = new Date(env.GAME_GLOBAL_START_T);
+  const globalEndAt = new Date(env.GAME_GLOBAL_END_T);
+  if (globalEndAt <= new Date()) {
+    throw new Error('GAME_GLOBAL_END_T must be in the future');
+  }
   return {
-    globalStartAt: new Date(env.GAME_GLOBAL_START_T),
-    globalEndAt: new Date(env.GAME_GLOBAL_END_T),
+    globalStartAt,
+    globalEndAt,
   };
 }
 
