13bb52f4b50e3ab5b3c9cba8b6036b301444dd52
OregSamSas <101597941+OregSamSas@users.noreply.github.com>
2026-08-31 12:03:56 +0200

    Adopt dropdown language switcher if we want to introduce more languages (probably not, so it might be only wise to use this one for consistency reasons)

--- apps/strategy-practice/src/language/language-selector.tsx
+++ apps/strategy-practice/src/language/language-selector.tsx
@@ -1,20 +1,16 @@
 import { useLanguage } from 'engine/react';
+import { LanguageDropdown } from 'common-frontend';
 
 export const LanguageSelector = () => {
   const { language, setLanguage } = useLanguage();
+
   return (
-    <span className="text-sm whitespace-nowrap">
-      <button
-        onClick={() => setLanguage('hu')}
-        className={`px-1 ${language === 'hu' ? 'font-bold' : 'opacity-40 hocus:opacity-70'}`}
-        aria-label="Magyar"
-      >HU</button>
-      <span className="opacity-40" aria-hidden="true">|</span>
-      <button
-        onClick={() => setLanguage('en')}
-        className={`px-1 ${language === 'en' ? 'font-bold' : 'opacity-40 hocus:opacity-70'}`}
-        aria-label="English"
-      >EN</button>
-    </span>
+    <LanguageDropdown
+      language={language}
+      onLanguageChange={setLanguage}
+      fontColor="white"
+      borderColor="transparent"
+      size="small"
+    />
   );
 };
